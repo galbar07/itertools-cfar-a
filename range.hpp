fb4a2cc110e36cf2b607531fa86dbe4f47ba5779
root <root@localhost.localdomain>
2020-06-10 11:26:24 -0400

    adding hpp files

---
+++ range.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+namespace itertools{
+
+    // vector<int> range(int v1,int v2){
+    //         vector<int> vect;  
+    //         for(int i=v1;i<v2;i++){
+    //             vect.push_back(i); 
+    //         }
+
+    //     return vect;
+    // }
+}
